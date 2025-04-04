3113b34334ef3df70d38600aca8aad9d29c6977f
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-04 22:18:10 +0700

    feat: add more role testing

--- tests/Feature/Auth/RoleTest.php
+++ tests/Feature/Auth/RoleTest.php
@@ -11,6 +11,32 @@ test('role can be assigned to user', function () {
     $adminRole = Role::firstOrCreate(['name' => Role::ROLE_ADMIN]);
 
     $user->roles()->attach($adminRole->id);
+    $user = $user->load('roles');
 
     expect($user->hasRole(Role::ROLE_ADMIN))->toBeTrue();
+});
+
+test('return false is user does not have the role', function() {
+    $user = User::factory()->create();
+    $user = $user->load('roles');
+
+    $adminRole = Role::firstOrCreate(['name' => Role::ROLE_ADMIN]);
+
+    $user->roles()->attach($adminRole->id);
+
+    expect($user->hasRole(Role::ROLE_USER))->toBeFalse();
+});
+
+test('role can be unassigned from user', function() {
+    $user = User::factory()->create();
+
+    $adminRole = Role::firstOrCreate(['name' => Role::ROLE_ADMIN]);
+
+    $user->roles()->attach($adminRole->id);
+    $user = $user->load('roles');
+    expect($user->hasRole(Role::ROLE_ADMIN))->toBeTrue();
+
+    $user->roles()->detach($adminRole->id);
+    $user = $user->load('roles');
+    expect($user->hasRole(Role::ROLE_ADMIN))->toBeFalse();
 });
