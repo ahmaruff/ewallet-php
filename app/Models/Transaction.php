6a7beb1bd2652b92bac939b8650fe9abd4916e6d
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-05 06:16:10 +0700

    feat: create transaction model and migration

---
+++ app/Models/Transaction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasUlids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'wallet_id',
+        'type',
+        'amount',
+        'description',
+        'reference_id',
+        'meta'
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'meta' => 'array',
+        ];
+    }
+}
