ff792e33d24cf352aa7114d08f9e6059e599e600
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-04 22:24:41 +0700

    feat: update auth unit test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
+
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
 
@@ -16,4 +19,46 @@ test('new users can register', function () {
 
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
+});
+
+test('new users can register and are assigned the default role', function () {
+    $defaultRoleName = Role::ROLE_USER;
+
+    $response = $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('dashboard', absolute: false));
+
+    $user = User::where('email', 'test@example.com')->first();
+    $this->assertNotNull($user);
+
+    $user = $user->load('roles');
+
+    // Assert that the user has the default role
+    $this->assertTrue($user->hasRole($defaultRoleName));
+});
+
+test('new users can register and are assigned the admin role', function () {
+    $response = $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('dashboard', absolute: false));
+
+    $user = User::where('email', 'test@example.com')->first();
+    $this->assertNotNull($user);
+
+    $user = $user->load('roles');
+
+    // Assert that the user has the admin role
+    $this->assertTrue($user->hasRole(Role::ROLE_ADMIN));
 });
