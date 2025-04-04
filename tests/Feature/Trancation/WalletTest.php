a82f7e79878a7f7ac695776460b2c9d3e5ae0a20
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-05 06:53:04 +0700

    feat: update test rules

--- tests/Feature/Trancation/WalletTest.php
+++ tests/Feature/Trancation/WalletTest.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Models\User;
+use App\Models\Wallet;
+use App\Services\WalletService;
 
 test('create wallet for the user', function() {
     $user = User::factory()->create();
-    expect($user->wallet)->isNotEmpty();
+    $walletService = app(WalletService::class);
+
+    $wallet = $walletService->createWalletForUser($user);
+
+    expect($wallet)->toBeInstanceOf(Wallet::class);
+    expect($wallet->user_id)->toBe($user->id);
+    expect((string) $wallet->balance)->toBe('0.00'); // Assuming default balance.  compare as string
 });
