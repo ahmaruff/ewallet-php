8abfe34cce8c42cb443ae18510c3c8ff7b55cfa7
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-05 12:46:21 +0700

    feat: update dashboard UI

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,7 +2,13 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import {Card, CardHeader, CardContent, CardTitle } from '../components/ui/card';
+import { Button } from '../components/ui/button';
+import { WalletMinimal, HandCoins } from 'lucide-vue-next';
+
+const props = defineProps<{
+    balance: number
+}>();
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,6 +16,7 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/dashboard',
     },
 ];
+
 </script>
 
 <template>
@@ -17,19 +24,28 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+            <div class="grid auto-rows-min gap-4 md:grid-cols-2">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>
+                            Balance
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <h1 class="text-4xl font-bold">{{ props.balance.toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 }) }}</h1>
+                    </CardContent>
+                </Card>
+                <div class="flex flex-col justify-center items-start gap-4">
+                    <Button variant="outline" @click="showDepositModal = true" >
+                        <WalletMinimal /> Deposit
+                    </Button>
+                    <Button variant="secondary">
+                        <HandCoins /> Withdraw
+                    </Button>
                 </div>
             </div>
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                <PlaceholderPattern />
+                
             </div>
         </div>
     </AppLayout>
