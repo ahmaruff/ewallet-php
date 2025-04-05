ef60ea9261ee46de608c53436151f082d64ca78e
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-05 08:11:15 +0700

    feat: create deposit test

--- tests/Feature/Trancation/WalletTest.php
+++ tests/Feature/Trancation/WalletTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Transaction;
 use App\Models\User;
 use App\Models\Wallet;
 use App\Services\WalletService;
@@ -26,4 +27,38 @@ test('create wallet for the user - returns null if wallet already exists', funct
     $secondWallet = $walletService->createWalletForUser($user);
 
     expect($secondWallet)->toBeNull();
+});
+
+test('user deposit money, then balance increased', function() {
+    $user = User::factory()->create();
+    $walletService = app(WalletService::class);
+
+    $wallet = $walletService->createWalletForUser($user,0.00);
+
+    $amount = 100000.00;
+
+    $transaction = $walletService->depositFunds($wallet, $amount, 'user deposit 100.000');
+
+    expect($transaction)->toBeInstanceOf(Transaction::class);
+    expect($transaction->type)->toBe(Transaction::TYPE_DEPOSIT);
+    expect((string) $transaction->amount)->toBe('100000.00');
+    $wallet->refresh();
+
+    expect($wallet->balance)->toBe('100000.00');
+});
+
+test('user deposit money less than or equal to 0, throw error amount', function () {
+    $user = User::factory()->create();
+    $walletService = app(WalletService::class);
+
+    $wallet = $walletService->createWalletForUser($user, 0.00);
+    $amount = 0;
+
+    expect(fn () => $walletService->depositFunds($wallet, $amount, 'user deposit 0'))
+        ->toThrow(Exception::class, 'Amount value cannot be less than or equal to 0');
+
+    $amount = -50000;
+
+    expect(fn () => $walletService->depositFunds($wallet, $amount, 'user deposit -50000'))
+        ->toThrow(Exception::class, 'Amount value cannot be less than or equal to 0');
 });
