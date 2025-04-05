f1ca4bdc3f5f3ae06adb6dc018f81d52b04e2c60
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-05 12:46:09 +0700

    feat: create dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
