94493e2ff49ec7d988ab7f52663263e83c5f09b4
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-04-05 20:39:52 +0700

    feat: transaction api

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(
+        ['user' => $request->user()]
+    );
 })->middleware('auth:sanctum');
 
 Route::group([
